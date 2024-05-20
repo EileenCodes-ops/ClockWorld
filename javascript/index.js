8316a27059dba82a13c84291eddc8e3e0cb5749f
EileenCodes-ops <145449239+EileenCodes-ops@users.noreply.github.com>
2024-05-20 15:39:58 +0100

    update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -34,22 +34,21 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = (
+  citiesElement.innerHTML = `
     <div class="city">
       <div>
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM   Do YYYY")}</div>
       </div>
       <div class="time">
-        {" "}
         ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
       </div>
     </div>
-  );
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
 
-let citiesSelectElement = document.querySelector("#city");
+let citiesSelectElement = document.querySelector("#select-city");
 citiesSelectElement.addEventListener("change", updateCity);
